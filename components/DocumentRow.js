aff4206b425ed46ef10ec2a292862858078d5593
Andre Smith <andre.frank.smith@gmail.com>
2022-08-21 21:51:29 +0200

    Implemented document row

--- components/DocumentRow.js
+++ components/DocumentRow.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Button } from "@material-tailwind/react";
+import { useRouter } from "next/router";
 
 function DocumentRow({ fileName, id, date }) {
+  const router = useRouter();
+
   return (
-    <div>
-      <p>{fileName}</p>
-      <p>{date?.toDate().toLocaleDateString()}</p>
+    <div
+      onClick={() => router.push(`/doc/${id}`)}
+      className="flex items-center p-4 rounded-lg hover:bg-grey-100 cursor-pointer"
+    >
+      <i className="material-icons text-blue-500 text-3xl">article</i>
+      <p className="flex-grow pl-5 w-10 pr-10 truncate font-bold">{fileName}</p>
+      <p className="pr-5 text-sm font-bold">
+        {date?.toDate().toLocaleDateString()}
+      </p>
+      <Button
+        color="grey"
+        variant="outlined"
+        ripple={true}
+        className="border-0"
+      >
+        <i className="material-icons cursor-pointer">more_vert</i>
+      </Button>
     </div>
   );
 }
--- components/MyDocuments.js
+++ components/MyDocuments.js
@@ -3,12 +3,13 @@ import DocumentRow from "./DocumentRow";
 
 function MyDocuments({ data }) {
   return (
-    <div className="flex items-center justify-between pb-5 ">
-      <h2 className="font-medium flex-grow">My Documents</h2>
-      <p className="mr-12">Date Created</p>
-      <i className="material-icons text-3xl text-grey-500">folder</i>
-
-      <div>
+    <>
+      <div className="flex items-center justify-between pb-5 ">
+        <h2 className="font-medium flex-grow">My Documents</h2>
+        <p className="mr-12">Date Created</p>
+        <i className="material-icons text-3xl text-grey-500">folder</i>
+      </div>
+      <div className="flex-row">
         {data?.map((doc) => (
           <>
             <DocumentRow
@@ -20,7 +21,7 @@ function MyDocuments({ data }) {
           </>
         ))}
       </div>
-    </div>
+    </>
   );
 }
 
