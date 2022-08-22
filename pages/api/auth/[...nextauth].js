d63dccea50b9b5ce92ba51dcc6fe0238b9544f41
Andre Smith <andre.frank.smith@gmail.com>
2022-08-22 21:10:03 +0200

    fixed page issue

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-import { db } from "../../../firebase";
 
 export default NextAuth({
   providers: [
